8a743da9cb4f1da44e77b647f7b5303d26ef9de3
Marco Savo <savomarco@gmail.com>
2021-12-27 12:02:15 +0100

    Update chatgui.h

--- src/chatgui.h
+++ src/chatgui.h
@@ -29,7 +29,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+   const ChatLogic *GetChatLogicHandle() const { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
